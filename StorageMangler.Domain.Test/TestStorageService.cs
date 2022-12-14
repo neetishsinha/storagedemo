fa50807ec47b4fc9ce5e94a89e582b88d61114b4
DigitalWebX <neerajsimha95@gmail.com>
2022-12-14 23:20:30 +0530

    TestStorageServiceModified

--- StorageMangler.Domain.Test/TestStorageService.cs
+++ StorageMangler.Domain.Test/TestStorageService.cs
@@ -1,17 +1,21 @@
-﻿using System.Threading.Tasks;
-using Castle.Components.DictionaryAdapter;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using NUnit.Framework;
+using StorageMangler.Domain.Infrastructure;
+using StorageMangler.Domain.Service;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace StorageMangler.Domain.Test
 {
+    [TestClass()]
     public class TestStorageService
     {
-
-        [Test]
-        [Ignore("no reason")]
-        public async Task TestSomething()
+        [TestMethod()]
+        public void TestSomething()
         {
-
+            var _storageServie = new Mock<IStorageService>();
+            Assert.IsNotNull(_storageServie.SetupAllProperties());
         }
     }
 }
